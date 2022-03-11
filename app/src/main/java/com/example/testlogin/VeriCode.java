3e8aa427c5bbc41d1baaa35647ecf0d196f75511
CongHau <comrades102@gmail.com>
2022-03-11 12:47:50 +0700

    login+signup+verifition code

---
+++ app/src/main/java/com/example/testlogin/VeriCode.java
@@ -0,0 +1,14 @@
+package com.example.testlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VeriCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veri_code);
+    }
+}
