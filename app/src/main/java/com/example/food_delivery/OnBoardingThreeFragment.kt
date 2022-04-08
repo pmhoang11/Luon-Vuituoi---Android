b9ede53abf9bed2f7b0e0e75f7f394445059dedc
NguyennHoang <nnhoang.ld@gmail.com>
2022-04-09 00:01:52 +0700

    fragment_error

---
+++ app/src/main/java/com/example/food_delivery/OnBoardingThreeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.food_delivery
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.food_delivery.databinding.FragmentOnBoardingThreeBinding
+import com.example.food_delivery.databinding.FragmentOnBoardingTwoBinding
+
+
+class OnBoardingThreeFragment : Fragment() {
+    lateinit var binding : FragmentOnBoardingThreeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentOnBoardingThreeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
